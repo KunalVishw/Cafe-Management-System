66e950f763511a5f6f1e1a9601f289878f4e938e
Kunal Vishwakarma <kunal.v2769@gmail.com>
2023-09-10 15:10:06 +0530

    started working on dashboard page

--- frontend/src/app/shared/global-constants.ts
+++ frontend/src/app/shared/global-constants.ts
@@ -2,6 +2,9 @@ export class GlobalConstants {
     // message
     public static genericError: string = "Something went wrong , please try again later";
 
+    public static unauthorized: string = "You are not authorized person to access this page.";
+    
+
     // regex
     public static nameRegex: string = "[a-zA-Z0-9 ]*";
 
