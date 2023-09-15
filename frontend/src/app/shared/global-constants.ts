2fc9c0a38ee1fe4eec9f9f22b57cce3d4e135265
Kunal Vishwakarma <kunal.v2769@gmail.com>
2023-09-15 13:18:28 +0530

    full project working fine

--- frontend/src/app/shared/global-constants.ts
+++ frontend/src/app/shared/global-constants.ts
@@ -4,6 +4,11 @@ export class GlobalConstants {
 
     public static unauthorized: string = "You are not authorized person to access this page.";
     
+    public static productExistError: string = "Product Already Exists.";
+
+    public static productAdded: string = "Product Added Successfully.";
+
+
 
     // regex
     public static nameRegex: string = "[a-zA-Z0-9 ]*";
